c87b9503f0c365209011294d433a599ccf67fba9
KtorZ <matthias.benkort@gmail.com>
2025-03-14 15:18:59 +0100

    wrap-up documentation

--- README.md
+++ README.md
@@ -13,16 +13,71 @@ yarn add @cardano-ogmios/mdk
 
 ## Usage
 
-```js
-import { ogmios, Json } from '@cardano-ogmios/mdk';
+### General usage
 
-const result = await ogmios((ws, done) => {
-  ws.once('message', (data) => {
-    done(Json.parse(data).result);
+```js
+import { ogmios, Json } from "@cardano-ogmios/mdk";
+
+const tip = await ogmios((ws, done) => {
+  ws.once("message", (data) => {
+    done(Json.parse(data).result.tip)
   });
 
-  ws.rpc('queryLedgerState/tip');
+  ws.rpc("findIntersection", { points: [ "origin" ] });
+});
+
+console.log(tip);
+```
+
+### Simple state-query
+
+```js
+import { ogmios, Json } from "@cardano-ogmios/mdk";
+
+const result = await ogmios((ws, done) => {
+  ws.queryLedgerState("tip").then(done);
 });
 
 console.log(Json.stringify(result));
 ```
+
+### Get a particular block child
+
+```js
+import { ogmios, Json } from "@cardano-ogmios/mdk";
+
+await ogmios(async (ws, done) => {
+  const chainFollower = await ws.newChainFollower(
+    [
+      {
+        id: "3d6f139f9f019668fe0412cacfaeb9e0be42e7b0f6ab21d6bddbc12d771ec18a",
+        slot: 86268539,
+      },
+    ],
+    1
+  );
+
+  for await (const { block } of chainFollower()) {
+    console.log(block);
+  }
+
+  done();
+});
+```
+
+### Follow the chain from the tip
+
+```js
+import { ogmios, Json } from "@cardano-ogmios/mdk";
+
+await ogmios(async (ws, done) => {
+  const chainFollower = await ws.newChainFollower();
+
+  for await (const { block } of chainFollower()) {
+    console.log(block);
+  }
+
+  done();
+});
+```
+
--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -1,17 +1,98 @@
 import test from "ava";
 import { ogmios, Json } from "../lib/index.mjs";
 
-test("🎉 query tip", async (t) => {
-  const result = await ogmios((ws, done) => {
+test("🎉 fetch tip", async (t) => {
+  const response = await ogmios((ws, done) => {
     ws.once("message", (data) => {
-      done(Json.parse(data).result);
+      done(Json.parse(data));
     });
 
-    ws.rpc("queryLedgerState/tip");
+    ws.rpc("findIntersection", { points: [ "origin" ] }, "myRequest");
   });
 
-  t.is(typeof result.id, "string");
-  t.is(typeof result.slot, "number");
+  t.is(typeof response.result.tip.id, "string");
+  t.is(typeof response.result.tip.slot, "number");
+  t.is(response.id, "myRequest");
+});
+
+test("🎉 query tip", async (t) => {
+  const tip = await ogmios((ws, done) => {
+    ws.queryLedgerState("tip").then(done);
+  });
+
+  t.is(typeof tip.id, "string");
+  t.is(typeof tip.slot, "number");
+});
+
+test("🎉 query network", async (t) => {
+  const time = await ogmios((ws, done) => {
+    ws.queryNetwork("startTime").then(done);
+  });
+
+  t.true(Number.isInteger(Date.parse(time)));
+});
+
+test("🎉 fetch child block", async (t) => {
+  const child = await ogmios(async (ws, done) => {
+    const chainFollower = await ws.newChainFollower(
+      [
+        {
+          id: "3d6f139f9f019668fe0412cacfaeb9e0be42e7b0f6ab21d6bddbc12d771ec18a",
+          slot: 86268539,
+        },
+      ],
+      1
+    );
+
+    for await (const { block } of chainFollower()) {
+      done(block);
+    }
+  });
+
+  t.is(child.id, "3d339c834346ec80ed08e67e4d0910c605ea9afdec39ed1c19e57944341517ce");
+});
+
+test("🎉 await next block (unlimited)", async (t) => {
+  t.timeout(120 * 1000, "no new block produced within a 2min timeframe...");
+
+  const tip = await ogmios(async (ws, done) => {
+    const chainFollower = await ws.newChainFollower();
+    for await (const { block } of chainFollower()) {
+      done(block);
+    }
+  });
+
+  t.is(typeof tip.id, "string");
+  t.is(typeof tip.slot, "number");
+});
+
+test("🎉 await next block (only one)", async (t) => {
+  t.timeout(120 * 1000, "no new block produced within a 2min timeframe...");
+
+  const tip = await ogmios(async (ws, done) => {
+    const chainFollower = await ws.newChainFollower(1);
+    for await (const { block } of chainFollower()) {
+      done(block);
+    }
+  });
+
+  t.is(typeof tip.id, "string");
+  t.is(typeof tip.slot, "number");
+});
+
+
+
+test("☠️ invalid query", async (t) => {
+  const result = await ogmios(async (ws, done) => {
+    try {
+      await ws.queryLedgerState("foo");
+      done("success");
+    } catch (_) {
+      done("failure");
+    }
+  });
+
+  t.is(result, "failure");
 });
 
 test("☠️ non-existing connection", async (t) => {
@@ -20,3 +101,65 @@ test("☠️ non-existing connection", async (t) => {
     message: /connect ECONNREFUSED/,
   });
 });
+
+test("☠️ invalid newChainFollower start", async (t) => {
+  await t.throwsAsync(async () => {
+    const error = await ogmios(async (ws, done) => {
+      try {
+        await ws.newChainFollower("foo", 42);
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
+
+    if (error) {
+      throw error;
+    }
+  }, {
+    any: true,
+    message: /expected an Array/,
+  });
+});
+
+
+test("☠️ invalid newChainFollower count", async (t) => {
+  await t.throwsAsync(async () => {
+    const error = await ogmios(async (ws, done) => {
+      try {
+        await ws.newChainFollower(["origin"], "bar");
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
+
+    if (error) {
+      throw error;
+    }
+  }, {
+    any: true,
+    message: /expected an Integer/,
+  });
+});
+
+test("☠️ invalid newChainFollower intersection", async (t) => {
+  await t.throwsAsync(async () => {
+    const error = await ogmios(async (ws, done) => {
+      try {
+        await ws.newChainFollower([{ id: "0000", slot: 0 }]);
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
+
+    if (error) {
+      throw error;
+    }
+  }, {
+    any: true,
+    message: /Invalid request/,
+  });
+});
+
